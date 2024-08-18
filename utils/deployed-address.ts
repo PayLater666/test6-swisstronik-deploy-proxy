0d78d1af0bd3e89390c7818742d23c5478dfa3ae
PayLater <76560973+PayLater666@users.noreply.github.com>
2024-08-19 00:17:11 +0700

    Update deployed-address.ts

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xF97fF79D5EADd8BB2869CA2857c3e176D08cBE4b'
+export const ProxyAdmin = '0xA9Bc165999C4051D4538a22D822160c2565a1a7c'
+export const SWTRImplementation = '0x837E0775A75B82864C095757343201ffE479Dc1b'
     
